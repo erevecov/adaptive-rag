b4a1b6fc0836b938d8b6e7b5f843e872b72db9a4
eduardo <eduardorevecovillalobos@gmail.com>
2026-06-19 16:59:42 -0400

    m7 ignore empty provider env

--- src/adaptive_rag/config/settings.py
+++ src/adaptive_rag/config/settings.py
@@ -13,6 +13,7 @@ class Settings(BaseSettings):
         env_prefix="ADAPTIVE_RAG_",
         env_file=".env",
         env_file_encoding="utf-8",
+        env_ignore_empty=True,
         extra="ignore",
     )
 
